c10ca1d3dae7dd298c298a6f80b93e381d604dc8
GitDoppler <carabatraducu@gmail.com>
2023-12-10 12:43:56 +0200

    removed test

--- app/(tabs)/_layout.jsx
+++
@@ -1,9 +0,0 @@
-import { Tabs } from "expo-router";
-
-export default function TabsLayout() {
-    return (
-        <Tabs screenOptions={{ headerShown: false }}>
-            <Tabs.Screen name="home" options={{ tabBarLabel: "Home", title: "Home" }} />
-        </Tabs>
-    );
-}
