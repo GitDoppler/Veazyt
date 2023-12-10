0e6243171346eb4b5f28a90477d2c49ca1fc6acd
GitDoppler <carabatraducu@gmail.com>
2023-12-10 13:23:36 +0200

    Added navbar

--- app/home/index.jsx
+++ app/home/index.jsx
@@ -4,6 +4,10 @@ import { Pressable, TextInput, View, Text } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import PromptSearch from "../../components/PromptSearch";
 import { Stack } from "expo-router";
+import HomeSVG from "../../components/svg-components/HomeSVG";
+import SearchSVG from "../../components/svg-components/SearchSVG";
+import PlaneSVG from "../../components/svg-components/PlaneSVG";
+import MoreSVG from "../../components/svg-components/MoreSVG";
 
 export default function Home() {
     const topInsets = useSafeAreaInsets().top;
@@ -13,22 +17,33 @@ export default function Home() {
                 <View className="z-2 flex-1 flex-col" style={{ paddingTop: topInsets ? topInsets + 10 : 20 }}>
                     <PromptSearch />
                 </View>
+                <View className=" h-[100px] w-full bg-[#FF8C20] self-end flex flex-row justify-around">
+                    <View className="flex flex-col justify-center items-center">
+                        <HomeSVG width={30} height={30} />
+                        <Text className="text-white text-base" style={{ fontFamily: "NotoBold" }}>
+                            Home
+                        </Text>
+                    </View>
+                    <View className="flex flex-col justify-center items-center">
+                        <SearchSVG width={30} height={30} />
+                        <Text className="text-white text-base" style={{ fontFamily: "NotoBold" }}>
+                            Toures
+                        </Text>
+                    </View>
+                    <View className="flex flex-col justify-center items-center">
+                        <PlaneSVG width={30} height={30} />
+                        <Text className="text-white text-base" style={{ fontFamily: "NotoBold" }}>
+                            Trips
+                        </Text>
+                    </View>
+                    <View className="flex flex-col justify-center items-center">
+                        <MoreSVG width={30} height={30} />
+                        <Text className="text-white text-base" style={{ fontFamily: "NotoBold" }}>
+                            More
+                        </Text>
+                    </View>
+                </View>
             </View>
         </>
     );
 }
-
-// export default function Home() {
-//     const [city, setCity] = useState("");
-//     const router = useRouter();
-//     return (
-//         <View className="flex-1 bg-white justify-center items-center">
-//             <View className="w-full px-3 flex flex-col items-center gap-4">
-//                 <TextInput onChangeText={setCity} value={city} placeholder="What city do you wish to visit ?" className="border-2 border-[#1E5AFF] rounded-xl text-black text-base p-3 w-full" />
-//                 <Pressable className="rounded-xl p-4 bg-[#1E5AFF]" onPress={() => router.push(`/city/${city}`)}>
-//                     <Text className="text-lg text-white font-bold text-center">Search</Text>
-//                 </Pressable>
-//             </View>
-//         </View>
-//     );
-// }
